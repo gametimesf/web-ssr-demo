cab94ab88baa3529b501634be13147c733e6a28a
iggy villamar <iggy@ivstudio.com>
2024-07-02 17:47:09 -0400

    add NotFound dummy component

---
+++ src/routes/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>We can't seem to find the page you're looking for.</p>
+    </div>
+  );
+}
