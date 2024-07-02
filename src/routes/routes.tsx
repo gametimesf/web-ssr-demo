a4d0fe2e83a5def37e1c6c9f445372d5bba57907
iggy villamar <iggy@ivstudio.com>
2024-07-02 18:00:28 -0400

    add testing routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import { Route, createRoutesFromElements } from 'react-router';
+import React from "react";
+import { Route, createRoutesFromElements } from "react-router";
 
-import { Container } from './Container'
-import { Home } from './Home'
-import { Page } from './Page'
-import { WithLoader } from './WithLoader'
-import { WithLoaderChild } from './WithLoaderChild'
+import { Container } from "./Container";
+import { Home } from "./Home";
+import { Page } from "./Page";
+import { WithLoader } from "./WithLoader";
+import { WithLoaderChild } from "./WithLoaderChild";
+import { PathValidator } from "./utils/PathValidator";
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<Container />}>
     <Route index element={<Home />} />
     <Route path="page" element={<Page />} />
-    <Route path="loader" element={<WithLoader />} loader={WithLoader.loader}>
-      <Route path="child" element={<WithLoaderChild />} loader={WithLoaderChild.loader} />
+    <Route path="search" loader={WithLoader.loader} element={<WithLoader />} />
+    <Route path="login" loader={WithLoader.loader} element={<WithLoader />} />
+    <Route
+      path="/:eventType/:eventName/:eventDate/events/:eventId"
+      loader={WithLoader.loader}
+      element={<WithLoader />}
+    >
+      <Route
+        path="listings/:listingId"
+        loader={WithLoaderChild.loader}
+        element={<WithLoaderChild />}
+      />
     </Route>
+
+    <Route
+      path=":slug"
+      loader={WithLoader.loader}
+      element={<PathValidator element={WithLoader} />}
+    />
   </Route>
 );
