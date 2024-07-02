63ed52023432034b9ee233c0bbe574156f0ea701
iggy villamar <iggy@ivstudio.com>
2024-07-02 17:55:12 -0400

    add logs to WithLoader page

--- src/routes/WithLoader/WithLoader.tsx
+++ src/routes/WithLoader/WithLoader.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useLoaderData, Outlet } from 'react-router-dom';
+import React from "react";
+import { useLoaderData, Outlet, useLocation } from "react-router-dom";
 
-import { search } from 'services';
-import { SearchResponse } from 'types';
-import { sleep } from 'utils';
+import { search } from "services";
+import { SearchResponse } from "types";
+import { sleep } from "utils";
 
-import styles from './WithLoader.module.css';
+import styles from "./WithLoader.module.css";
 
 interface LoaderResult {
   message: string;
@@ -14,19 +14,21 @@ interface LoaderResult {
 
 export function WithLoader() {
   const data = useLoaderData() as LoaderResult | undefined;
+  const location = useLocation();
 
   return (
     <div className={styles.withLoader}>
+      <h2>Location: {location.pathname}</h2>
       <span>{data?.message}</span>
       <Outlet />
     </div>
-  )
+  );
 }
 WithLoader.loader = async (): Promise<LoaderResult> => {
   await sleep();
 
   return {
     message: `WithLoader loaded on ${__RUNTIME_ENVIRONMENT__}`,
-    data: await search('warriors'),
+    data: await search("warriors"),
   };
-}
+};
